758c032f6b0510dc9660930248b1cb37a7f6d62d
dani3laM <d.martinezr2@uniandes.edu.co>
2025-02-07 21:58:53 -0500

    fin

--- src/Fila.java
+++ src/Fila.java
@@ -3,16 +3,20 @@ import java.util.ArrayList;
 public class Fila {
     private ArrayList<int[]> filaClientes;
     private boolean terminado = false;
+
     public Fila(){
         filaClientes = new ArrayList<int[]>();
     }
+
     public ArrayList<int[]> getClientes(){
         return this.filaClientes;
     }
+    
     public synchronized void ingresarFila(int[]cliente){
         filaClientes.add(cliente);
         notifyAll();
     }
+
     public synchronized int[] salirFila(){
         while (filaClientes.isEmpty() && !terminado) {
             try {
@@ -27,8 +31,8 @@ public class Fila {
         }
         return filaClientes.remove(0);
     }
+
     public synchronized void marcarFinalizacion() {
         terminado = true;
-        notifyAll();
     }
 }
